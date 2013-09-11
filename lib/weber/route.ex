37afcf9838e1f76cdfc3b92f61f2458961d47ff8
0xAX <testtest.89@mail.ru>
2013-09-12 01:02:37 +0600

    indentation fixed

--- lib/weber/route.ex
+++ lib/weber/route.ex
@@ -1,25 +1,25 @@
 defmodule Weber.Route do
 
-  @moduledoc """
-    This module handles routing of weber web application.
-    Use 'route' macros for declaring routing.
+    @moduledoc """
+      This module handles routing of weber web application.
+      Use 'route' macros for declaring routing.
 
-    route when('/', Controller1, Action1)
-              |> routes ('', Controller2, Action2)
-              |> routes ('', Controller2, Action1)
-              |> otherwise (404, Controller2, ActionNotFound)
-  """
+      route when('/', Controller1, Action1)
+                |> routes ('', Controller2, Action2)
+                |> routes ('', Controller2, Action1)
+                |> otherwise (404, Controller2, ActionNotFound)
+    """
 
-  @doc """
-  """
-  defmacro route(body) do          
-        quote do:     
-            unquote((fn(body) -> body end).(body))        
-  end
+    @doc """
+    """
+    defmacro route(body) do          
+          quote do:     
+              unquote((fn(body) -> body end).(body))        
+    end
 
-  def routes() do
-  end
+    def routes() do
+    end
 
-  def otherwise do
-  end
+    def otherwise do
+    end
 end
