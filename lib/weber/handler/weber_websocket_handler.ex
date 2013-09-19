6672fbd7692cb17db0b43b2f4a1fb4965419177a
0xAX <testtest.89@mail.ru>
2013-09-20 00:37:15 +0600

    #5 initial websocket handler

--- lib/weber/handler/weber_websocket_handler.ex
+++ lib/weber/handler/weber_websocket_handler.ex
@@ -1,3 +1,18 @@
 defmodule Handler.WeberWebSocketHandler do
     
+    def init(_, _req, _opts) do
+        {:upgrade, :protocol, :cowboy_websocket}
+    end
+
+    def websocket_init(_, req, _opts) do
+        {:ok, req, []}
+    end
+
+    def websocket_handle(data, req, state) do
+        {:ok, req, state}
+    end
+
+    def websocket_terminate(reason, req, state) do
+        :ok
+    end
 end
