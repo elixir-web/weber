4e51fa23ff1ef06d9e0913ec2f5dcfe8c5ea8ebe
Matheus Caceres <matheuscaceres@gmail.com>
2013-09-24 14:01:24 -0300

    Helper.Html test

--- test/weber_helper_html_test.exs
+++ test/weber_helper_html_test.exs
@@ -13,8 +13,30 @@ defmodule WeberHelperHtmlTest do
     assert(html == "<p class=\"test_class\" id=\"test_id\">test</p>")
   end
 
-  test "Create tag with attributes" do
+  test "Create tag with attributes boolean" do
     html = tag(:p, "test", [class: "test_class", id: "test_id", required: true])
     assert(html == "<p class=\"test_class\" id=\"test_id\" required>test</p>")
+  end
+
+  test "Create tag with another tags inside" do
+    html = tag(:div) do
+      tag(:p, "test", [class: "test"])
+    end
+    assert(html == "<div><p class=\"test\">test</p></div>")
+  end
+
+  test "Create tag with attributes and another tags inside" do
+    html = tag(:div, [class: "div_test", id: "id_test"]) do
+      tag(:span, [class: "span_class"]) do
+        tag(:p, "test", [class: "test"])
+      end
+    end
+
+    assert(html == "<div class=\"div_test\" id=\"id_test\"><span class=\"span_class\"><p class=\"test\">test</p></span></div>")
+  end
+
+  test "Create tags without end" do
+    html = tag(:img, [src: "path/to/file.png"])
+    assert(html == "<img src=\"path/to/file.png\">")
   end
 end
