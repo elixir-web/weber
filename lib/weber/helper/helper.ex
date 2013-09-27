96b502a75e15a0822cb5da02d79d0fc3f1a32a1d
Matheus Caceres <matheuscaceres@gmail.com>
2013-09-27 09:07:14 -0300

    Weber.Helper.include_view documentation

--- lib/weber/helper/helper.ex
+++ lib/weber/helper/helper.ex
@@ -1,7 +1,23 @@
 defmodule Weber.Helper do
+  @moduledoc """
+  This module provides helpers functions to help you in views.
+  """
+
+  @doc """
+  Returns the view `file`.
+  You can use include_view inside your views.
+
+  Usage: 
+  include_view("path/file")
+  Or
+  include_view("path/file", [value: value])
+
+  If you need files outside views path, use as: include_view("/complete/path/file") 
+  """
   def include_view(file, args // []) do
     views_path = :gen_server.call(Mix.Project.get, :views)
     file = file |> Path.expand(views_path)
+
     EEx.eval_file(file, args)
   end
 end
