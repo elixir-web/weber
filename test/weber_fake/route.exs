8e7ab50bc27e1800e1b4524b00d8adbeddc7edfb
Matheus Caceres <matheuscaceres@gmail.com>
2013-10-15 13:46:42 -0300

    Add controller, routes and statics html file.

---
+++ test/weber_fake/route.exs
@@ -0,0 +1,11 @@
+Code.require_file "lib/controllers/test.exs", __DIR__
+
+defmodule Example.Route do
+  import Weber.Route
+
+  @route on("/", :Example.Test, :render_site)
+
+  def get_route do
+    @route
+  end
+end
