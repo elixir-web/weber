2651db1e75b5dc64031f143e7f03ec4016dd2f02
0xAX <testtest.89@mail.ru>
2013-10-16 00:10:49 +0600

    [Session] API for sessions added

--- lib/weber/session/session.ex
+++ lib/weber/session/session.ex
@@ -7,6 +7,8 @@ defmodule Weber.Session do
 
   use GenServer.Behaviour
 
+  import Weber.Http.Params
+
   defrecord Session,
     session_life_time: nil,
     session_id:  nil
@@ -23,6 +25,51 @@ defmodule Weber.Session do
   def handle_info({:time_to_die, session_id}, state) do
     :ets.delete(:cookie_storage, session_id)
     {:stop, :normal, state}
+  end
+
+  def get_session do
+    case get_session_helper() do
+      [] -> []
+      [{_, _, opts}] -> opts
+    end
+  end
+
+  def get_session_val(key) do
+    case get_session_helper() do
+      [] -> []
+      [{_, _, opts}] -> 
+        val = :lists.keyfind(key, 1, opts)
+        case val do
+          false -> []
+          {_, v} -> v
+        end
+    end
+
+  end
+
+  def set_session_val(key, val) do
+    case get_session_helper() do
+      [] -> []
+      [{s, pid, opts}] ->
+        :ets.delete(:cookie_storage, s)
+        :ets.insert(:cookie_storage, {s, pid, [{key, val} | opts]})
+        :ok
+    end
+  end
+
+  defp get_session_helper do
+    cookie = get_cookie("weber")
+
+    case cookie do
+      :undefined -> []
+      _ ->
+        sessions_list = :ets.tab2list(:cookie_storage)
+        Enum.filter(sessions_list, 
+          fn({s, _, _}) ->
+            :erlang.list_to_binary(:lists.concat(:lists.concat(:erlang.binary_to_list(s)))) == cookie
+          end)
+    end
+
   end
 
 end
