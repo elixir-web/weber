2cced1c48a7a3bf83a38900b8397263e51ae8767
Matheus Caceres <matheuscaceres@gmail.com>
2013-10-21 15:37:58 -0200

    Add test for views has bindings.

--- test/helper/weber_helper_content_for_test.exs
+++ test/helper/weber_helper_content_for_test.exs
@@ -17,6 +17,13 @@ defmodule WeberHelperContentForTest do
     assert file_content =~ %r/Hello/
   end
 
+  test "content for layout bindings" do
+    file_content = File.read!(app_views("test_bind.html"))
+    Weber.Helper.ContentFor.content_for(:layout)
+
+    assert file_content =~ %r/<%= value %>/
+  end
+
   defp app_views(file) do
     __DIR__ <> "/../weber_fake/lib/views/" <> file
   end
