6e679996e7e93f1355f0965c1bc578335786a41c
Matheus Caceres <matheus@formaweb.com.br>
2013-10-28 00:30:24 -0200

    Remove layout context from controller.ex; Add layout macro in Weber.Controller.Layout

---
+++ lib/weber/controller/layout.ex
@@ -0,0 +1,17 @@
+defmodule Weber.Controller.Layout do
+  @moduledoc """
+  This module contains layout macro defined. It's useful to change which layout controller needs.
+    ##Example
+    layout 'test.html'
+  """
+  
+  @doc "Create layout function"
+  defmacro layout(path) do
+    quote do
+      def unquote(:__layout__)() do
+        unquote(path)
+      end
+      defoverridable [__layout__: 0]
+    end
+  end
+end
