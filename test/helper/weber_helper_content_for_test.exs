a8ea2aa68f02d02598db89b418d1e06b3b111e37
Matheus Caceres <matheus@formaweb.com.br>
2013-10-28 23:47:44 -0200

    Add test when its a false path

--- test/helper/weber_helper_content_for_test.exs
+++ test/helper/weber_helper_content_for_test.exs
@@ -22,6 +22,12 @@ defmodule WeberHelperContentForTest do
     assert file_content =~ %r/<%= value %>/
   end
 
+  test "when not use a layout" do
+    file_content = File.read!(app_views("test.html"))
+    Weber.Helper.ContentFor.content_for(:layout, false)
+    assert file_content == "Hello\n"
+  end
+
   defp app_views(file) do
     __DIR__ <> "/../weber_fake/lib/views/" <> file
   end
