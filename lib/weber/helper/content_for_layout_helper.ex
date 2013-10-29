7673a4f34aa35268e88d2646471ce9bdc58079f2
Matheus Caceres <matheus@formaweb.com.br>
2013-10-28 23:48:36 -0200

    Add condition to verification if path isn't false

--- lib/weber/helper/content_for_layout_helper.ex
+++ lib/weber/helper/content_for_layout_helper.ex
@@ -3,11 +3,16 @@ defmodule Weber.Helper.ContentFor do
   This module create content_for.
   Please note for your layouts just put <%= content_for_layout %>
   """
+
+  @doc "Variable file_content changes to layout correct and view render"
   defmacro content_for(:layout, path) do
     quote do
-      views = :gen_server.call(Mix.Project.get.project[:app], :views)
-      layout_path = String.from_char_list!(views ++ 'layouts/' ++ unquote(path))
-      var!(file_content) = EEx.eval_file(layout_path, [content_for_layout: var!(file_content)])
+      if unquote(path) do
+        views = :gen_server.call(Mix.Project.get.project[:app], :views)
+        layout_path = String.from_char_list!(views ++ 'layouts/' ++ unquote(path))
+        var!(file_content) = EEx.eval_file(layout_path, [content_for_layout: var!(file_content)])
+      end
     end
   end
+
 end
