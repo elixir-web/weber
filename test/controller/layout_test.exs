3cab9a5373d81e623410338664c3d8dbed59027c
Matheus Caceres <matheus@formaweb.com.br>
2013-10-28 23:43:18 -0200

    Weber.Controller.layout test

--- test/controller/layout_test.exs
+++ test/controller/layout_test.exs
@@ -1,7 +1,7 @@
 defmodule WeberControllerLayoutTest do
   use ExUnit.Case
 
-  defmodule Controller.Example do
+  defmodule Controller.Example1 do
     use Weber.Controller
 
     layout 'test.html'
@@ -11,11 +11,21 @@ defmodule WeberControllerLayoutTest do
     use Weber.Controller
   end
 
+  defmodule Controller.Example3 do
+    use Weber.Controller
+
+    layout false
+  end
+
   test "take layout file correct" do
-    assert Controller.Example.__layout__  == 'test.html'
+    assert Controller.Example1.__layout__  == 'test.html'
   end
 
   test "take layout file standard" do
     assert Controller.Example2.__layout__  == 'main.html'
+  end
+
+  test "don't use layout for controller" do
+    assert Controller.Example3.__layout__  == false
   end
 end
