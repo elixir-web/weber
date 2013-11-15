62b5c99adfab8e7da1f9be160565e71f75fe8e38
0xAX <testtest.89@mail.ru>
2013-11-15 23:35:32 +0600

    Some logs

--- lib/weber/handler/weber_req_handler_result.ex
+++ lib/weber/handler/weber_req_handler_result.ex
@@ -23,7 +23,7 @@ defmodule Handler.WeberReqHandler.Result do
     :io.format("filename ~p~n", [filename])
     :io.format("app.views ~p~n", [app.views])
     :io.format("get_all_files(app.views) ~p~n", [get_all_files(app.views)])
-    :io.format("find_file_path(get_all_files(app.views), filename)) ~p~n", [find_file_path(get_all_files(app.views), filename))])
+    :io.format("find_file_path(get_all_files( ~p~n", [find_file_path(get_all_files(app.views), filename)])
 
     :io.format("build_module_name(:lists.nth(1, find_file_path(get_all_files(app.views), filename))) ~p~n", [build_module_name(:lists.nth(1, find_file_path(get_all_files(app.views), filename)))])
 
