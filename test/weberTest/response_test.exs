eea60e4f1d3a9e4da0b94648d503031bffd28264
0xAX <testtest.89@mail.ru>
2013-12-03 00:43:17 +0600

    Update response_test.exs

--- test/weberTest/response_test.exs
+++ test/weberTest/response_test.exs
@@ -3,14 +3,14 @@ defmodule WeberHttpResponseTest do
    
   test "SimpleResponse test" do
     {:ok, status, _, client} = :hackney.request(:get, 'http://localhost:8080/weber', [], <<>>, [])
-    {:ok, body, _} = :hackney.body(client)
+    body = :hackney.body(client)
     assert(body == {:ok, "Main\n"})
     assert(status == 200)
   end
 
   test "SimpleResponse Include test" do
     {:ok, status, _, client} = :hackney.request(:get, 'http://localhost:8080/include', [], <<>>, [])
-    {:ok, body, _} = :hackney.body(client)
+    body = :hackney.body(client)
     assert(body == {:ok, "Main\n"})
     assert(status == 200)
   end
