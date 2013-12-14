c38ce9e6eff0031e4099b9b5169611a373bebdb3
Josh Adams <josh@isotope11.com>
2013-12-13 18:34:30 -0600

    Remove unnecessary whitespace in templates

--- templates/default/mix.exs
+++ templates/default/mix.exs
@@ -2,7 +2,7 @@ defmodule #{projectNamespace}.Mixfile do
   use Mix.Project
 
   def project do
-    [ 
+    [
       app: :#{projectName},
       version: "0.0.1",
       deps: deps(Mix.env)
@@ -17,17 +17,16 @@ defmodule #{projectNamespace}.Mixfile do
   end
 
   defp deps(:prod) do
-    [ 
-      { :weber, github: "0xAX/weber" } 
+    [
+      { :weber, github: "0xAX/weber" }
     ]
   end
 
   defp deps(:test) do
     deps(:prod) ++ [{ :hackney, github: "benoitc/hackney" }]
   end
-  
+
   defp deps(_) do
     deps(:prod)
   end
-
 end
