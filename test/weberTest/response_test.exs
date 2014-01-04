c7624c0011c528fc02c3d8913b2bbf46527631e9
Dickson S. Guedes <guedes@guedesoft.net>
2014-01-04 10:56:45 -0200

    Revert "fix test errors"
    This reverts commit d4876946dbfe4732eb02c7fc49ea8c8afd40e21b.

--- test/weberTest/response_test.exs
+++ test/weberTest/response_test.exs
@@ -1,29 +1,29 @@
 defmodule WeberHttpResponseTest do
   use ExUnit.Case
-
+   
   test "SimpleResponse test" do
     {:ok, status, _, client} = :hackney.request(:get, 'http://localhost:8080/weber', [], <<>>, [])
-    {:ok, body, _rest } = :hackney.body(client)
-    assert(body == "Main\n")
+    body = :hackney.body(client)
+    assert(body == {:ok, "Main\n"})
     assert(status == 200)
   end
 
   test "SimpleResponse Include test" do
     {:ok, status, _, client} = :hackney.request(:get, 'http://localhost:8080/include', [], <<>>, [])
-    {:ok, body, _rest } = :hackney.body(client)
-    assert(body == "Main\n")
+    body = :hackney.body(client)
+    assert(body == {:ok, "Main\n"})
     assert(status == 200)
   end
 
   test "`redirect` in route test" do
-    {:ok, status, _, _client} = :hackney.request(:get, 'http://localhost:8080/redirect', [], <<>>, [])
+    {:ok, status, _, client} = :hackney.request(:get, 'http://localhost:8080/redirect', [], <<>>, [])
     assert(status == 302)
   end
 
   test "`content_for` test" do
-    {:ok, _status, _, client} = :hackney.request(:get, 'http://localhost:8080/content_for', [], <<>>, [])
-    {:ok, body, _rest } = :hackney.body(client)
-    assert(body == "<!DOCTYPE html>\n<html>\n  <head>\n    <title>\n      My Project\n    </title>\n    <meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\" />\n  </head>\n  <body>\n    <div id=\"container\">\n    Hello Weber!      \n    </div>\n  </body>\n</html> ")
+    {:ok, status, _, client} = :hackney.request(:get, 'http://localhost:8080/content_for', [], <<>>, [])
+    body = :hackney.body(client)
+    assert(body == {:ok, "<!DOCTYPE html>\n<html>\n  <head>\n    <title>\n      My Project\n    </title>\n    <meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\" />\n  </head>\n  <body>\n    <div id=\"container\">\n    Hello Weber!      \n    </div>\n  </body>\n</html> "})
   end
 
 end
