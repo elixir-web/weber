84ab4902c4703f87bed413e6edac3ff89d9fa9aa
0xAX <testtest.89@mail.ru>
2014-02-16 00:24:05 +0600

    refactoring

--- lib/weber/cowboy.ex
+++ lib/weber/cowboy.ex
@@ -1,5 +1,5 @@
 defmodule Cowboy do
-    
+
   @moduledoc """
     Weber's weber server.
   """
@@ -8,45 +8,34 @@ defmodule Cowboy do
   Start new cowboy http/web socket handler with given name and config
   from config.ex
   """
-  def start(config) do
-    :application.start(:crypto)
-    :application.start(:public_key)
-    :application.start(:ssl)
-    :application.start(:mimetypes)
-    :application.start(:ranch)
-    :application.start(:cowlib)
-    :application.start(:cowboy)
-        
-    {:webserver, web_server_config} = :lists.keyfind(:webserver, 1, config)
-    {_, _host}     = :lists.keyfind(:http_host, 1, web_server_config)
-    {_, port}      = :lists.keyfind(:http_port, 1, web_server_config)
-    {_, acceptors} = :lists.keyfind(:acceptors, 1, web_server_config)
-    {_, ssl}       = :lists.keyfind(:ssl, 1, web_server_config)
-    case :lists.keymember(:use_gzip, 1, web_server_config) do
-      true -> {_, compress} = :lists.keyfind(:use_gzip, 1, web_server_config)
-      _    -> compress = false
-    end
-    
-    dispatch = case :lists.keyfind(:ws, 1, config) do
-      {:ws, ws_config} ->
-        {_, ws_mod}  = :lists.keyfind(:ws_mod, 1, ws_config)
-        :cowboy_router.compile([{:_, [{'/_ws', Handler.WeberWebSocketHandler, ws_mod}, 
-                                      {'/[...]', Handler.WeberReqHandler, config}]}])
-      _ ->
-        :cowboy_router.compile([{:_, [{'/[...]', Handler.WeberReqHandler, config}]}])
-    end
-     
-    case ssl do
-      true -> 
-        {_, cacertifile} = :lists.keyfind(:cacertfile_path, 1, web_server_config)
-        {_, certfile} = :lists.keyfind(:certfile_path, 1, web_server_config)
-        {_, keyfile} = :lists.keyfind(:keyfile_path, 1, web_server_config)
-        {:ok, _} = :cowboy.start_https(:https, acceptors, [{:port, port}, {:cacertfile, cacertifile},
-                                                           {:certfile,certfile}, {:keyfile, keyfile}], 
-                                                           [env: [dispatch: dispatch]])
-      _ -> 
-        {:ok, _} = :cowboy.start_http(:http, acceptors, [port: port], [env: [dispatch: dispatch],
-                                                                       compress: compress])
+  @dependencies [:crypto, :public_key, :ssl, :mimetypes, :ranch, :cowlib, :cowboy]
+
+  def start(config, handler) do
+
+    Enum.map(@dependencies, &( :application.start(&1) ) )
+
+    web_server_config = Keyword.fetch!(config, :webserver)
+
+    [_host, port, acceptors, ssl] =
+      [:http_host, :http_port, :acceptors, :ssl]
+        |> Enum.map( &Keyword.fetch!(web_server_config, &1) )
+    ws_mod = config |> Keyword.fetch!(:ws) |> Keyword.fetch!(:ws_mod)
+
+    compress = Keyword.get(web_server_config, :use_gzip, false)
+
+    dispatch = :cowboy_router.compile([{:_, [{'/_ws', Handler.WeberWebSocketHandler, ws_mod},
+                                             {'/[...]', Handler.WeberReqHandler, {config, handler}}]}])
+
+    if ssl do
+        [cacertifile, certfile, keyfile] =
+          [:cacertfile_path, :certfile_path, :keyfile_path]
+            |> Enum.map( &Keyword.fetch!(web_server_config, &1) )
+        {:ok, _} = :cowboy.start_https(:https, acceptors, [port: port, cacertfile: cacertifile,
+                                                           certfile: certfile, keyfile: keyfile,
+                                                           env: [dispatch: dispatch]])
+    else
+      :cowboy.start_http(:http, acceptors, [port: port], [env: [dispatch: dispatch],
+                                                          compress: compress])
     end
   end
 end
